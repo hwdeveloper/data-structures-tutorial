865d8662ca9e0fc3816e11b1ca2d5b1cb2e09b08
[seraph] <[hw_developer@163.com]>
2018-06-04 17:28:46 +0800

    添加代码

--- 02栈/03栈应用/Stack.js
+++ 02栈/03栈应用/Stack.js
@@ -1,24 +1,22 @@
 function Stack() {
     this.dataStore = [];
     this.top = 0;
-    this.push = push;
-    this.pop = pop;
-    this.peek = peek;
-    this.length=length;
-    this.clear=clear;
-}
-function push(element) {
-    this.dataStore[this.top++] = element;
-}
-function pop() {
-    return this.dataStore[--this.top];
-}
-function peek() {
-    return this.dataStore[this.top-1];
-}
-function length() {
-    return this.top;
-}
-function clear() {
-    this.top = 0;
+    this.push = function (element) {
+        this.dataStore[this.top++] = element;
+    }
+    this.pop = function () {
+        return this.dataStore[--this.top];
+    }
+
+    this.peek= function() {
+        return this.dataStore[this.top-1];
+    }
+    this.length=function () {
+        return this.top;
+    }
+
+    this.clear=function() {
+        this.dataStore=[];
+        this.top = 0;
+    }
 }
